5b14f7aa11ecd091fd1381c2c61652363753042e
unknown <spacemanspiff@lutean.com>
2018-03-13 19:02:05 -0700

    added meta

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: safari-zone-basics
+# about: A collection of small tweaks for the Safari Zone forum.
+# version: 0.1
+# authors: Flower_Child
+# url: https://github.com/tipsypastels/safari-zone-basics
