5e09c3717fe90d8e60f9648079e5472ad39ed037
unknown <spacemanspiff@lutean.com>
2018-03-15 20:59:45 -0700

    a new strategy

--- plugin.rb
+++ plugin.rb
@@ -7,20 +7,31 @@
 after_initialize do
   About.class_eval do
 
-    STAFF_GROUPS = [ # todo make this IDs
-      "Higher_Staff",
-      "SZ_Moderators",
-      "Battle_Server_Staff",
-      "Development_Staff",
-      "Discord_Staff",
-      "Social_Birbs",
-      "Zine_Staff"
-    ]
-    def moderators
-      @moderators = STAFF_GROUPS.map do |group_name|
-        Group.find_by(name: group_name)
-      end
+    # STAFF_GROUPS = [ # todo make this IDs
+    #   "Higher_Staff",
+    #   "SZ_Moderators",
+    #   "Battle_Server_Staff",
+    #   "Development_Staff",
+    #   "Discord_Staff",
+    #   "Social_Birbs",
+    #   "Zine_Staff"
+    # ]
+    # def moderators
+    #   @moderators = STAFF_GROUPS.map do |group_name|
+    #     Group.find_by(name: group_name)
+    #   end
+    # end
+
+    def admins
+      @admins = Group.find(55).users
+        .human_users
+        .order(:username_lower)
     end
 
+    def moderators
+      @moderators = Group.find(44).users
+        .human_users
+        .order(:username_lower)
+    end
   end
 end
