8fc9655531b34cdd3f712a71e766a4a19b75fcb0
unknown <spacemanspiff@lutean.com>
2018-03-15 22:20:26 -0700

    generalized, added all groups

--- plugin.rb
+++ plugin.rb
@@ -4,45 +4,62 @@
 # authors: Flower_Child
 # url: https://github.com/tipsypastels/safari-zone-basics
 
+
+#@HACK clean this up and DRY up code
 after_initialize do
   About.class_eval do
-    attr_accessor :developers
 
-    # STAFF_GROUPS = [ # todo make this IDs
-    #   "Higher_Staff",
-    #   "SZ_Moderators",
-    #   "Battle_Server_Staff",
-    #   "Development_Staff",
-    #   "Discord_Staff",
-    #   "Social_Birbs",
-    #   "Zine_Staff"
-    # ]
-    # def moderators
-    #   @moderators = STAFF_GROUPS.map do |group_name|
-    #     Group.find_by(name: group_name)
-    #   end
-    # end
+    STAFF_GROUPS = [
+      # exclude admins and mods bc they're already defined
+      :bss,
+      :developers,
+      :discord_staff,
+      :social_media,
+      :zine_staff
+    ]
 
-    def admins
-      @admins = Group.find(55).users
-        .human_users
-        .order(:username_lower)
+    attr_accessor *STAFF_GROUPS
+
+    def admins # actually hstaff
+      @admins = find_group(55)
     end
 
     def moderators
-      @moderators = Group.find(44).users
-        .human_users
-        .order(:username_lower)
+      @moderators = find_group(44)
+    end
+
+    def bss
+      @bss = find_group(53)
     end
 
     def developers
-      @developers = Group.find(47).users
+      @developers = find_group(47)
+    end
+
+    def discord_staff
+      @discord_staff = find_group(45)
+    end
+
+    def social_media
+      @social_media = find_group(51)
+    end
+
+    def zine_staff
+      @zine_staff = find_group(49)
+    end
+
+    private
+
+    def find_group(id)
+      Group.find(id).users
         .human_users
-        .order(:username_lower)
+        .order(:username_lowere)
     end
   end
 
   AboutSerializer.class_eval do
-    has_many :developers, serializer: UserNameSerializer, embed: :objects
+    About::STAFF_GROUPS.each do |group|
+      has_many group, serializer: UserNameSerializer, embed: :objects
+    end
   end
 end
