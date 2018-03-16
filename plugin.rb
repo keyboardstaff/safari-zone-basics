0b02717d79bd8b068bc56cde48cee4e8fcbd7a85
unknown <spacemanspiff@lutean.com>
2018-03-15 23:21:44 -0700

    metaprogramming

--- plugin.rb
+++ plugin.rb
@@ -8,67 +8,16 @@
 #@HACK clean this up and DRY up code
 after_initialize do
   About.class_eval do
-
-    STAFF_GROUPS = [
-      # exclude admins and mods bc they're already defined
-      :bss,
-      :developers,
-      :discord_staff,
-      :social_media,
-      :zine_staff
-    ]
-
-    attr_accessor *STAFF_GROUPS
-
-    def admins # actually hstaff
-      @admins = find_group(55)
-    end
-
-    def moderators
-      @moderators = find_group(44)
-    end
-
-    def bss
-      @bss = find_group(53)
-    end
-
-    def developers
-      @developers = find_group(47)
-    end
-
-    def discord_staff
-      @discord_staff = find_group(45)
-    end
-
-    def social_media
-      @social_media = find_group(51)
-    end
-
-    def zine_staff
-      @zine_staff = find_group(49)
-    end
-
-    private
-
-    def find_group(id)
-      Group.find(id).users
-        .human_users
-        .order(:username_lower)
+    attr_accessor :groups
+    # inefficient but probably okay as long as there aren't that many groups
+    def groups
+      @groups = Group.all.select do |group|
+        group.custom_fields['display_on_staff'] == 't'
+      end
     end
   end
 
   AboutSerializer.class_eval do
-    STAFF_GROUPS = [
-      # exclude admins and mods bc they're already defined
-      :bss,
-      :developers,
-      :discord_staff,
-      :social_media,
-      :zine_staff
-    ]
-
-    STAFF_GROUPS.each do |group|
-      has_many group, serializer: UserNameSerializer, embed: :objects
-    end
+    has_many :groups, embed: :objects
   end
 end
