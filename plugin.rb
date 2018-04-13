3aef7611277dfce2ab5b532517b0ba59ae82b0c5
unknown <spacemanspiff@lutean.com>
2018-04-13 02:15:31 -0700

    category topic query

--- plugin.rb
+++ plugin.rb
@@ -77,15 +77,14 @@ after_initialize do
     end
   end
 
-  PostSerializer.class_eval do
-    def primary_group_name
-      return nil unless object.user && object.user.primary_group_id
+  TopicQuery.class_eval do
+    def list_category_topic_ids(category)
+      query = default_results(category: category.id)
+      pinned_ids = query.where('pinned_at IS NOT NULL AND category_id = ?', category.id)
+                        .order('pinned_at DESC').pluck(:id)
+      non_pinned_ids = query.where('pinned_at IS NULL OR category_id = ?', category.id).pluck(:id)
 
-      if @topic_view
-        @topic_view.primary_group_names[object.user.primary_group_id]
-      else
-        object.user.primary_group.full_name if object.user.primary_group
-      end
+      (pinned_ids + non_pinned_ids)[0...@options[:per_page]]
     end
   end
 end
