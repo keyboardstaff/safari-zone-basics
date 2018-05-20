4d0777753d3e1243aa2bb9b65b4bf5f47c358ee0
unknown <spacemanspiff@lutean.com>
2018-05-20 02:52:47 -0700

    subcategory slug serializer

--- plugin.rb
+++ plugin.rb
@@ -7,6 +7,94 @@
 #@HACK clean this up and DRY up code
 after_initialize do
 
+  Category.class_eval do
+    attr_accessor :subcategory_slugs
+  end
+
+  CategoryDetailedSerializer.class_eval do
+    attributes :subcategory_slugs
+
+    def include_subcategory_slugs?
+      subcategory_slugs.present?
+    end
+  end
+
+  CategoryList.class_eval do
+    private def find_categories
+      @categories = Category.includes(
+        :uploaded_background,
+        :uploaded_logo,
+        :topic_only_relative_url,
+        subcategories: [:topic_only_relative_url]
+      ).secured(@guardian)
+
+      @categories = @categories.where("categories.parent_category_id = ?", @options[:parent_category_id].to_i) if @options[:parent_category_id].present?
+
+      if SiteSetting.fixed_category_positions
+        @categories = @categories.order(:position, :id)
+      else
+        @categories = @categories.order('COALESCE(categories.posts_week, 0) DESC')
+          .order('COALESCE(categories.posts_month, 0) DESC')
+          .order('COALESCE(categories.posts_year, 0) DESC')
+          .order('id ASC')
+      end
+
+      @categories = @categories.to_a
+
+      category_user = {}
+      default_notification_level = nil
+      unless @guardian.anonymous?
+        category_user = Hash[*CategoryUser.where(user: @guardian.user).pluck(:category_id, :notification_level).flatten]
+        default_notification_level = CategoryUser.notification_levels[:regular]
+      end
+
+      allowed_topic_create = Set.new(Category.topic_create_allowed(@guardian).pluck(:id))
+      @categories.each do |category|
+        category.notification_level = category_user[category.id] || default_notification_level
+        category.permission = CategoryGroup.permission_types[:full] if allowed_topic_create.include?(category.id)
+        category.has_children = category.subcategories.present?
+      end
+
+      if @options[:parent_category_id].blank?
+        subcategories = {}
+        subcategory_slugs = {}
+        to_delete = Set.new
+        @categories.each do |c|
+          if c.parent_category_id.present?
+            subcategories[c.parent_category_id] ||= []
+            subcategories[c.parent_category_id] << c.id
+
+            subcategory_slugs[c.parent_category_id] ||= []
+            subcategory_slugs[c.parent_category_id] << c.slug
+            to_delete << c
+          end
+        end
+        @categories.each { |c| 
+          c.subcategory_ids = subcategories[c.id] 
+          c.subcategory_slugs = subcategory_slugs[c.id]
+        }
+        @categories.delete_if { |c| to_delete.include?(c) }
+      end
+
+      if @topics_by_category_id
+        @categories.each do |c|
+          topics_in_cat = @topics_by_category_id[c.id]
+          if topics_in_cat.present?
+            c.displayable_topics = []
+            topics_in_cat.each do |topic_id|
+              topic = @topics_by_id[topic_id]
+              if topic.present? && @guardian.can_see?(topic)
+                # topic.category is very slow under rails 4.2
+                topic.association(:category).target = c
+                c.displayable_topics << topic
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
   TopicListItemSerializer.class_eval do
     def first_poster_username
       posters.first.try(:user).try(:username)
